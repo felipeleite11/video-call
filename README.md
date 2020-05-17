7c9f1e34742f29b8d16d41681eb8fb7a8ee83825
felipeleite11 <lipi4shared@hotmail.com>
2020-05-17 09:40:25 -0300

    Project creation.

---
+++ README.md
@@ -0,0 +1,63 @@
+<h1 align="center">Video call with NodeJS</h1>
+
+This project implements WEB video calls using [WebRTC](https://webrtc.org), a technology focused on real-time communication via browser or native app.
+
+
+<h2>Environment requirements</h2>
+
+- [NodeJS](https://nodejs.org) with the [NPM](https://www.npmjs.com) package manager installed.
+- Install the [Yarn](https://yarnpkg.com) if you prefer to use it.
+
+<h2>Required NPM packages</h2>
+
+|  Módulo    | Objetivo                                                  |
+| -------------------------: | ----------------------------------------- |
+| `Express`  | Create a web server to serve statically the test page     |
+| `SocketIO` | Synchronizes events between callers                       |
+
+
+<h2>Features</h2>
+
+- Video and audio stream
+- Auto-start call
+- End the call
+- Shared text area, updated on all callers in real-time
+
+
+<h2>Get started</h2>
+
+- Clone the project: `git clone https://github.com/felipeleite11/video-call.git`
+- Execute instalation of dependencies: `npm install` ou `yarn`
+- Execute application: `npm start` ou `yarn start`
+- Open `http://localhost:3000/room_name` in your browser
+- **Allow the required permission** to access your **camera** and **microphone**
+- Open a anonymous instance of your browser and open the **same URL**
+- Allow the required permissions again
+
+
+<h2>The expected is...</h2>
+
+- Two identical images, side by side, captured directly from your webcam
+- When speaking into your microphone, your voice should be echoed through your device’s speaker
+
+
+<h2>Browser compatibility</h2>
+
+WebRTC project is compatible with most desktop and mobile browsers.
+
+- [X] Chrome
+- [X] Firefox
+- [X] Edge
+- [X] Safari
+- [X] Opera
+- [ ] Internet Explorer
+
+Check more support details in [Can I use](https://caniuse.com/#search=webrtc).
+
+
+<h2>Contribution suggestions</h2>
+
+- Test page styling
+- Call start from a button
+- Allow room creation
+- Improve responsiveness for mobile devices
---
+++ public/js/video.js
@@ -0,0 +1,149 @@
+(function() {
+  /** @type {SocketIOClient.Socket} */
+  const socket = io.connect(window.location.origin);
+  const localVideo = document.querySelector('.localVideo');
+  const remoteVideo = document.querySelector('.remoteVideo');
+  const textarea = document.querySelector('#textarea');
+  const endCall = document.querySelector('#end-call');
+  const peerConnections = {};
+
+  let room = !location.pathname.substring(1) ? 'home' : location.pathname.substring(1);
+  let getUserMediaAttempts = 5;
+  let gettingUserMedia = false;
+
+  /** @type {RTCConfiguration} */
+  const config = {
+    'iceServers': [{
+      'urls': ['stun:stun.l.google.com:19302']
+    }]
+  };
+
+  /** @type {MediaStreamConstraints} */
+  const constraints = {
+    audio: true,
+    video: { facingMode: "user" }
+  };
+
+  textarea.addEventListener('keyup', e => {
+    socket.emit('typing', e.target.value);
+  });
+
+  socket.on('full', function(room) {
+    alert('A sala ' + room + ' está cheia!');
+  });
+
+  socket.on('bye', function(id) {
+    console.log(`Socket ${id} desconectado!`);
+    handleRemoteHangup(id);
+  });
+
+  if (room && !!room) {
+    socket.emit('join', room);
+  }
+
+  window.onunload = window.onbeforeunload = function() {
+    socket.close();
+  };
+
+  socket.on('ready', function (id) {
+    if (!(localVideo instanceof HTMLVideoElement) || !localVideo.srcObject) {
+      return;
+    }
+    const peerConnection = new RTCPeerConnection(config);
+    peerConnections[id] = peerConnection;
+    if (localVideo instanceof HTMLVideoElement) {
+      peerConnection.addStream(localVideo.srcObject);
+    }
+    peerConnection.createOffer()
+    .then(sdp => peerConnection.setLocalDescription(sdp))
+    .then(function () {
+      socket.emit('offer', id, peerConnection.localDescription);
+    });
+    peerConnection.onaddstream = event => handleRemoteStreamAdded(event.stream, id);
+    peerConnection.onicecandidate = function(event) {
+      if (event.candidate) {
+        socket.emit('candidate', id, event.candidate);
+      }
+    };
+  });
+
+  socket.on('offer', function(id, description) {
+    const peerConnection = new RTCPeerConnection(config);
+    peerConnections[id] = peerConnection;
+    if (localVideo instanceof HTMLVideoElement) {
+      peerConnection.addStream(localVideo.srcObject);
+    }
+    peerConnection.setRemoteDescription(description)
+    .then(() => peerConnection.createAnswer())
+    .then(sdp => peerConnection.setLocalDescription(sdp))
+    .then(function () {
+      socket.emit('answer', id, peerConnection.localDescription);
+    });
+    peerConnection.onaddstream = event => handleRemoteStreamAdded(event.stream, id);
+    peerConnection.onicecandidate = function(event) {
+      if (event.candidate) {
+        socket.emit('candidate', id, event.candidate);
+      }
+    };
+  });
+
+  socket.on('candidate', function(id, candidate) {
+    peerConnections[id].addIceCandidate(new RTCIceCandidate(candidate))
+    .catch(e => console.error(e));
+  });
+
+  socket.on('answer', function(id, description) {
+    peerConnections[id].setRemoteDescription(description);
+  });
+
+  socket.on('typing', function(text) {
+    console.log(text);
+    textarea.value = text;
+  });
+
+  function getUserMediaSuccess(stream) {
+    gettingUserMedia = false;
+    if (localVideo instanceof HTMLVideoElement) {
+      !localVideo.srcObject && (localVideo.srcObject = stream);
+    }
+    socket.emit('ready');
+  }
+
+  function handleRemoteStreamAdded(stream, id) {
+    remoteVideo.srcObject = stream;
+    remoteVideo.setAttribute("id", id.replace(/[^a-zA-Z]+/g, "").toLowerCase());
+  }
+
+  function getUserMediaError(error) {
+    console.error(error);
+    gettingUserMedia = false;
+    (--getUserMediaAttempts > 0) && setTimeout(getUserMediaDevices, 1000);
+  }
+
+  function getUserMediaDevices() {
+    if (localVideo instanceof HTMLVideoElement) {
+      if (localVideo.srcObject) {
+        getUserMediaSuccess(localVideo.srcObject);
+      } else if (!gettingUserMedia && !localVideo.srcObject) {
+        gettingUserMedia = true;
+        navigator.mediaDevices.getUserMedia(constraints)
+          .then(getUserMediaSuccess)
+          .catch(getUserMediaError);
+      }
+    }
+  }
+
+  function handleRemoteHangup(id) {
+    peerConnections[id] && peerConnections[id].close();
+    delete peerConnections[id];
+    document.querySelector("#" + id.replace(/[^a-zA-Z]+/g, "").toLowerCase()).remove();
+  }
+
+  endCall.addEventListener('click', () => {
+    console.log('Desligando...');
+    socket.close();
+    remoteVideo.srcObject = null;
+  });
+
+  getUserMediaDevices();
+})();
